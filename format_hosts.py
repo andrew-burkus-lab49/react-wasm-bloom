af63adeb774c31d1da56b60656d4bc844bf30fcb
Andrew Burkus <andrew.burkus@lab49.com>
2023-05-11 14:32:30 -0400

    update hosts formatter

--- format_hosts.py
+++ format_hosts.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-path = "./public/hosts.csv"
+path = "./public/hosts.txt"
+out_path = "./public/hosts_full.csv"
 
 with open(path) as file:
     lines = [line.strip() for line in file if line.count("0.0.0.0") > 0]
     lines = [line.replace(" ", ",") for line in lines]
-    print(lines)
+    print(f"{len(lines)} lines")
+    for line in lines[:10]:
+        print(f"\t{line}")
 
-with open(path, "w") as file:
+with open(out_path, "w") as file:
     file.write("ip, host\n")
     print(f"Writing {len(lines)} lines")
     file.write("\n".join(lines))
